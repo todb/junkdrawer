cb89adbfa13e45ad65e60e994d6eb99b47084fdf
Tod Beardsley <todb@packetfu.com>
2014-04-08 12:26:48 -0500

    Add example usage

--- cardio.sh
+++ cardio.sh
@@ -2,6 +2,19 @@
 # A one-liner (hah!) for checking all listening services for static
 # links against openssl. This does briefly run under sudo (to get
 # accurate PID information). Tested on Debian-derived linuces.
+#
+# Example:
+# [sudo] password for todb:
+# $ ./cardio.sh
+# /opt/google/talkplugin/GoogleTalkPlugin: 1.0.1e 11 Feb 2013
+# /usr/lib/postgresql/9.1/bin/postgres: 1.0.0
+# /usr/lib/vmware/bin/vmware-hostd:
+# /usr/sbin/cupsd:
+#
+# Notice that GoogleTalkPlugin in 1.0.1e (not 1.0.1g or later)
+# and postgres is unaffected by the Heartbleed vuln:
+#
+# http://heartbleed.com/
 
 CHECKLISTENINGPROGS=$(CHECKLISTENINGPIDS=$(sudo netstat -nlp \
   | grep "LISTEN " | sed -e 's/.* \([0-9]\+\)\/.*/\1/');
