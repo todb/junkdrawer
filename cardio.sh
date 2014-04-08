df49e881002cabfd7b30c293cce92e11a624bd8c
Tod Beardsley <todb@packetfu.com>
2014-04-08 12:07:45 -0500

    Format cadio.sh to 80 columns

--- cardio.sh
+++ cardio.sh
@@ -3,4 +3,16 @@
 # links against openssl. This does briefly run under sudo (to get
 # accurate PID information). Tested on Debian-derived linuces.
 
-CHECKLISTENINGPROGS=$(CHECKLISTENINGPIDS=$(sudo netstat -nlp | grep "LISTEN " | sed -e 's/.* \([0-9]\+\)\/.*/\1/'); for i in $CHECKLISTENINGPIDS; do ps --no-headers -p $i -o command; done | sed -e 's/^[^\/]*//' | sed -e 's/ .*//' | sort -u) ; for a in $CHECKLISTENINGPROGS; do echo -n $a; echo -n ": " ; echo $(strings $a | grep -i 'openssl[ _-][0-9]' | sed -e 's/.*openssl[ _-]\([^ ]\)/\1/i') ; done; unset CHECKLISTENINGPROGS; unset CHECKLISTENINGPIDS
+CHECKLISTENINGPROGS=$(CHECKLISTENINGPIDS=$(sudo netstat -nlp \
+  | grep "LISTEN " | sed -e 's/.* \([0-9]\+\)\/.*/\1/');
+  for i in $CHECKLISTENINGPIDS;
+  do ps --no-headers -p $i -o command;
+  done | sed -e 's/^[^\/]*//' | \
+  sed -e 's/ .*//' | sort -u) ;
+  for a in $CHECKLISTENINGPROGS;
+  do echo -n $a; echo -n ": " ;
+  echo $(strings $a | grep -i 'openssl[ _-][0-9]' | \
+  sed -e 's/.*openssl[ _-]\([^ ]\)/\1/i') ;
+  done;
+  unset CHECKLISTENINGPROGS;
+  unset CHECKLISTENINGPIDS
