3455ab86e9d8bef49740e0c35faf657e1a04fece
Tod Beardsley <todb@packetfu.com>
2014-04-08 11:48:12 -0500

    Update comment docs for cardio.sh

--- cardio.sh
+++ cardio.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# A one-liner (hah!) for checking all listening services for static
+# links against openssl. This does briefly run under sudo (to get
+# accurate PID information). Tested on Debian-derived linuces.
 
 CHECKLISTENINGPROGS=$(CHECKLISTENINGPIDS=$(sudo netstat -nlp | grep "LISTEN " | sed -e 's/.* \([0-9]\+\)\/.*/\1/'); for i in $CHECKLISTENINGPIDS; do ps --no-headers -p $i -o command; done | sed -e 's/^[^\/]*//' | sed -e 's/ .*//' | sort -u) ; for a in $CHECKLISTENINGPROGS; do echo -n $a; echo -n ": " ; echo $(strings $a | grep -i 'openssl[ _-][0-9]' | sed -e 's/.*openssl[ _-]\([^ ]\)/\1/i') ; done; unset CHECKLISTENINGPROGS; unset CHECKLISTENINGPIDS
