0b2efa49bc9663215cf51a1d677149f9389c0e0f
Tod Beardsley <tod_beardsley@rapid7.com>
2015-02-11 10:37:27 -0600

    dirty search/replace on pr lists

---
+++ linkprs.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+infile = ARGV[0]
+outfile = infile + ".out"
+newfile = []
+link = 'https://github.com/rapid7/metasploit-framework/pull/'
+
+data = File.open(infile) {|f| f.read f.stat.size}
+data.each_line do |line|
+  if line =~ /(PR #(\d+)):/
+    whole_pr = $1
+    pr_num = $2
+    newfile << line.gsub(whole_pr, "<a href='#{link}#{pr_num}>#{whole_pr}</a>")
+  else
+    newfile << line
+  end
+end
+
+File.open(outfile, "wb") {|f| f.write newfile.join}
+
