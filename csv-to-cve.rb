2383b46665e3582d578951dabb5674c93273a3f7
Tod Beardsley <tod_beardsley@rapid7.com>
2019-04-17 11:10:00 -0400

    Skip already submitted to CNAs

--- csv-to-cve.rb
+++ csv-to-cve.rb
@@ -26,7 +26,7 @@ end
 cve_data = []
 
 csv.each do |line|
-  next if line["PR of JSON"] # Already submitted
+  next if line["PR of JSON"] || line["Contact CNA?"] # Already submitted
   next unless line["Reserve CVE"]
   new_cve = {
     :cve_id => line["Reserve CVE"],
